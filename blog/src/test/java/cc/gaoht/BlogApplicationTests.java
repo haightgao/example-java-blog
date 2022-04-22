79d72551d526daef2199514ca3e3fa49ea11eecb
haight <gao_ht@126.com>
2022-04-22 17:00:03 +0800

    add springboot

---
+++ blog/src/test/java/cc/gaoht/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.gaoht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
