1b26dd63f3ad743b1d99a9c6a19ee0d56a9f165c
haight <gao_ht@126.com>
2022-04-24 15:27:28 +0800

    完成模板绑定

--- blog/src/main/java/cc/gaoht/controller/IndexController.java
+++ blog/src/main/java/cc/gaoht/controller/IndexController.java
@@ -3,13 +3,19 @@ package cc.gaoht.controller;
 import cc.gaoht.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
-    @GetMapping("/")
+    @GetMapping("")
     public String index(){
 //        int i=9/0;
-        throw new NotFoundException("blog not found");
-//        return "index";
+//        throw new NotFoundException("blog not found");
+        return "index";
+    }
+
+    @GetMapping("/detail")
+    public String detail(){
+        return "blog";
     }
 }
---
+++ blog/src/main/resources/templates/README.md
@@ -0,0 +1,11 @@
+# 插件集成
+
+- [编辑器 Markdown](https://github.com/pandao/editor.md) [官网](http://editor.md.ipandao.com/)
+- [内容排版 typo.css](https://github.com/sofish/typo.css)
+- [动画 animate.css](https://github.com/animate-css/animate.css)
+- [代码高亮 prism](https://github.com/PrismJS/prism)
+- [滚动侦测 waypoints](https://github.com/imakewebthings/waypoints)
+- [平滑滚动 jquery.scrollTo](https://github.com/flesler/jquery.scrollTo)
+- [目录生成 Tocbot](https://github.com/tscanlin/tocbot)
+- [二维码生成 qrcode.js](https://github.com/davidshimjs/qrcodejs)
+
